0ec318a890182aa440c754755586ee0514536c93
tamizhselvangt <tamizhselvangt@gmail.com>
2024-12-14 21:26:13 +0530

    Initial

---
+++ FakeStoreAPI/Shared/Products/Model/Product.swift
@@ -0,0 +1,8 @@
+//
+//  Products.swift
+//  FakeStoreAPI
+//
+//  Created by Tamizhselvan gurusamy on 12/14/24.
+//
+
+import Foundation
---
+++ FakeStoreAPI/Shared/Products/ViewModel/ProductsViewModel.swift
@@ -0,0 +1,8 @@
+//
+//  ProductsViewModel.swift
+//  FakeStoreAPI
+//
+//  Created by Tamizhselvan gurusamy on 12/14/24.
+//
+
+import Foundation
