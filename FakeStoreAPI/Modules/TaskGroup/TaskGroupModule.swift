d3c50da0faee560272f2ad9190afe34444828f2c
tamizhselvangt <tamizhselvangt@gmail.com>
2024-12-15 16:48:15 +0530

    Final Commit

---
+++ FakeStoreAPI/Modules/TaskGroup/TaskGroupModule.swift
@@ -0,0 +1,18 @@
+//
+//  TaskGroupModule.swift
+//  FakeStoreAPI
+//
+//  Created by Tamizhselvan gurusamy on 12/15/24.
+//
+
+import SwiftUI
+
+struct TaskGroupModule: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    TaskGroupModule()
+}
